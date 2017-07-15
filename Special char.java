c988e62f25f13a509b427cf79bee09e0fb688552
keerthanajanani <keerthijanani66@gmail.com>
2017-07-15 11:59:58 +0530

    Create Special char.java

---
+++ Special char.java
@@ -0,0 +1,28 @@
+# Special-char
+import java.io.*;
+import java.util.*;
+public class Special char
+{
+public static void main(String args[])
+{
+int count=0,n;
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+ArrayList<String> a=new ArrayList<String>();
+ArrayList<String> b=new ArrayList<String>();
+System.out.println("Enter the special character:");
+n=Integer.parseInt(br.readLine());
+for(int i=0;i<a.length();i++)
+{
+a.add=(':');
+}
+for(int j=0;j<b.size();j++)
+{
+b.add=(',');
+}
+if(a.charAt(i)==((character)b.get(j)).charvalue())
+{
+count++;
+}
+System.out.println("special character is : "+count);
+}
+}
